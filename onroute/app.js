887dc244a0541d74f8535deeb72bf28b2c4e970a
Phoebe Quincy <phoebequincy@gmail.com>
2018-12-13 15:53:41 -0700

    Added route names to app.js and set up route files.

--- onroute/app.js
+++ onroute/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var eventsRouter = require('./routes/events');
+var vehiclesRouter = require('./routes/vehicles');
 
 var app = express();
 
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/events', eventsRouter);
+app.use('/vehicles', vehiclesRouter);
 
 module.exports = app;
